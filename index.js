99af75f43da580e3675177bf8def7684d353bf2d
Lovepreet <lovepreet.singh@medtrail.in>
2019-11-27 16:37:02 +0530

    wfbwef

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Connect to MongoDB
 mongoose
   .connect(
-    'mongodb://mongo:27017/docker-node-mongo',
+    'mongodb://127.0.0.1:27017/docker-node-mongo',
     { useNewUrlParser: true }
   )
   .then(() => console.log('MongoDB Connected'))
